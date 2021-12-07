48177b7d6c5a23cb7406ce1a2acde46e1c00fc94
Rick Brown <ricky.brown.00@gmail.com>
2021-12-07 14:16:08 +0000

    Initial Commit

---
+++ amazon-ui-redesign/amazon_ui_redesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  amazon_ui_redesignApp.swift
+//  amazon-ui-redesign
+//
+//  Created by Rick Brown on 07/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct amazon_ui_redesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
